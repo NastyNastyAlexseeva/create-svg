d27caf8bd7a6135e4fbf6e34bb48e5fd95e4f0f2
Nastya Alexseeva <n4stya.alexseeva@gmail.com>
2021-10-17 22:26:40 +0400

    add valid atributes

--- attributes.js
+++ attributes.js
@@ -25,7 +25,7 @@ export class Attributes extends Constructor {
 
     setElementsAttributes(elements) {
         if(elements && elements.length) {
-            const addAttributesElement = (element) => {
+            const watchElements = (element) => {
                 element.map(nestedEl => {
                     const { attributes, elements, id } = nestedEl;
 
@@ -34,55 +34,90 @@ export class Attributes extends Constructor {
                     if(!elements || !elements.length) {
                         this.setAttributes(id, attributes);
                     } else {
-                        addAttributesElement(elements);
+                        watchElements(elements);
                     }
                 });
             };
 
-            addAttributesElement(elements);
+            watchElements(elements);
         }
     }
 
     setValidationAttributesSvg(attrs) {
+        const { version, xmlns, viewBox, style, className,  width, height, fill, strokeWidth, strokeDasharray, strokeDashoffset, stroke } = attrs;
 
         Object.assign(attrs, {
-            version: this.setTypeValidation(attrs.version) ?? '1.1',
-            xmlns: this.setTypeValidation(attrs.xmlns) ?? "http://www.w3.org/2000/svg",
-            viewBox: this.setTypeValidation(attrs.viewBox),
-            width: this.setTypeValidation(attrs.width, ['string', 'number']),
-            height: this.setTypeValidation(attrs.height, ['string', 'number']),
-            fill:  this.setTypeValidation(attrs.fill),
-            'stroke-width': this.setTypeValidation(attrs.strokeWidth, ['string', 'number']),
-            stroke: this.setTypeValidation(attrs.stroke),
+            version: this.setValidation(version) ?? '1.1',
+            xmlns: this.setValidation(xmlns) ?? "http://www.w3.org/2000/svg",
+            viewBox: this.setValidation(viewBox),
+            style: this.setValidation(style),
+            class: this.setValidation(className),
+            width: this.setValidation(width, ['string', 'number']),
+            height: this.setValidation(height, ['string', 'number']),
+            fill:  this.setValidation(fill),
+            'stroke-width': this.setValidation(strokeWidth, ['string', 'number']),
+            'stroke-dasharray': this.setValidation(strokeDasharray),
+            'stroke-dashoffset': this.setValidation(strokeDashoffset, ['string', 'number']),
+            stroke: this.setValidation(stroke),
         })
 
         delete attrs.strokeWidth;
+        delete attrs.className;
         this.deleteInvalidAttributes(attrs);
     }
 
     setValidationAttributesElement(attrs) {
+        const { style, className, width, height, fill, fillRule, strokeWidth, stroke, strokeDasharray, strokeDashoffset, x, y, cx, cy, r, d } = attrs;
 
         Object.assign(attrs, {
-            width: this.setTypeValidation(attrs.width, ['string', 'number']),
-            height: this.setTypeValidation(attrs.height, ['string', 'number']),
-            fill:  this.setTypeValidation(attrs.fill),
-            'stroke-width': this.setTypeValidation(attrs.strokeWidth, ['string', 'number']),
-            stroke: this.setTypeValidation(attrs.stroke),
-            cx: this.setTypeValidation(attrs.cx, ['string', 'number']),
-            cy: this.setTypeValidation(attrs.cy, ['string', 'number']),
-            r: this.setTypeValidation(attrs.r, ['string', 'number']),
-            d: this.setTypeValidation(attrs.d),
+            style: this.setValidation(style),
+            class: this.setValidation(className),
+            width: this.setValidation(width, ['string', 'number']),
+            height: this.setValidation(height, ['string', 'number']),
+            fill:  this.setValidation(fill),
+            'fill-rule': this.setValidation(fillRule, [], ['nonzero', 'evenodd']),
+            'stroke-width': this.setValidation(strokeWidth, ['string', 'number']),
+            'stroke-dasharray': this.setValidation(strokeDasharray),
+            'stroke-dashoffset': this.setValidation(strokeDashoffset, ['string', 'number']),
+            stroke: this.setValidation(stroke),
+            x: this.setValidation(x, ['string', 'number']),
+            y: this.setValidation(y, ['string', 'number']),
+            cx: this.setValidation(cx, ['string', 'number']),
+            cy: this.setValidation(cy, ['string', 'number']),
+            r: this.setValidation(r, ['string', 'number']),
+            d: this.setValidation(d),
         })
 
+        delete attrs.fillRule;
+        delete attrs.strokeDasharray;
+        delete attrs.strokeDashoffset;
         this.deleteInvalidAttributes(attrs);
     }
 
-    setTypeValidation(value, types = ['string']) {
-        const typesValid = types.some(type => {
-            return typeof(value) === type;
-        })
+    setValidation(value, types = ['string'], validValues = []) {
+        let errorMessage = '';
 
-        return value && typesValid ? value : null;
+        if(!types.length) types.push('string');
+
+        const validType = types.some(type => {
+            return typeof(value) === type;
+        });
+
+        const validValue = validValues.some(validValue => {
+            return value === validValue;
+        });
+
+        if(value) {
+            if(validType && validValue || validType && !validValues.length) {
+                return value;
+            } else {
+                if(!validType) errorMessage = `Invalid type value attributes: expected ${types}`;
+                if(!validValue) errorMessage = `Invalid value attributes: expected ${validValues}`;
+                console.error(errorMessage);
+
+                return null;
+            };
+        };
 
     }
 
